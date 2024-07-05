76a70f5ed3daf4dd8c6869844fb517712141c83f
IgorAlexandrovich <igoralexandrovich95@mail.ru>
2024-07-05 20:01:49 +0500

    complete

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    let showAll = document.querySelector('.brands__button');
+    let brands = document.querySelector('.brands-slider');
+    showAll.addEventListener('click', () => {
+        showAll.classList.toggle('show');
+        brands.classList.toggle('show');
+        if (showAll.classList.contains('show')) {
+            showAll.querySelector('span').textContent = 'Скрыть все';
+        } else {
+            showAll.querySelector('span').textContent = 'Показать все';
+        }
+    });
+
+    if (window.innerWidth <= 550) {
+        document.querySelector(".swiper-container").classList.add('swiper');
+        document.querySelector(".brands-slider").classList.add('swiper-wrapper');
+        document.querySelectorAll(".brands-slider__item").forEach((item)=>{
+            item.classList.add('swiper-slide')
+        })
+        var swiper = new Swiper(".swiper-container", {
+            slidesPerView: "auto",
+            slidesOffsetBefore: 32,
+            spaceBetween: 16,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    }
+
+    // var swiper = new Swiper(".test", {
+    //     slidesPerView: "auto",
+    //     spaceBetween: 30,
+    //     pagination: {
+    //         el: ".swiper-pagination",
+    //         clickable: true,
+    //     },
+    // });
+});
+
+
